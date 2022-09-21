33b941c55b8d76e74a8f59becb0c9c07c629d573
materialymatematyka <101131190+materialymatematyka@users.noreply.github.com>
2022-09-21 19:45:35 +0200

    smooth

---
+++ assets/passwd/js/pozcrypt.js
@@ -0,0 +1,139 @@
+// variables to be filled when generating the file
+    var encryptedMsg = '9541ec6dab1fcdfd4b64504a62c0e012a03a1271c9f5ebeb2513e438a8b9f17468577b5d074d70d84ffeadfe06271a98U2FsdGVkX1/x6kQ7LPbu7+alIpYxKvLXmykEqJVtxSy7fwPChvtHN0Jpml6TK0ytJLrWi9f4akYSBpSeg2PRtsdF6cIt3ufYwLS1Ywt6VP4BQy3zxGvGqD06ZUcZ22j8hrbDNzNlOeujxmzAft+Vvz6p9uSGaPZ1Lfow8NMcTtqV+LSoGbfSEJBYITXP0CyiNqg09EkLm1axbOqxk8M14QKr5/H1swim3AkeKOXJPfum65eu6BiEsf8hYS0WUnDTX0InZHU8F1NHqyfQHxpviE17ZpxP7EI2Xe9lg045+4+IpwLtTG1zXZIlO8LUQlabGlyQPmjRCDSyfWVvnhKiuRfVhdwTFjboHql1Oc0Jpo7UD4/BzJ04uBJUu19OeK05cNT4LX95vpNadde9qXPecm4+5cuYKX7EsuEGvrH3QFM7DlHUnSrdVPmt7cnG6drbBR9/GXBUnVmkrRWxQrtYkuw2orpPSI9zGaCInc/jfpJZZG5+yp7Bxd9klLTdYAD8jWk86MvyNfOw1KyRfxY9/GVSuVOQjg1Ezhv13kGNu0ZiUKOGuySdQ4YKvBldYMsv18pxZPF+L1O3pPXET1Q2suAwbbBtNlhPMJ6Uj9ZC+4adZiMPvrBSGkYWWKHoGEBgqUqZN0+Vn4BiAropdZqs7bQD4JZVOClm2oqffyKUen/52BTIu5115SruXftPs9A58jue/nOPTYi/EjeVbOz9skIewLqlHqHxRxr/+rC3MCJQ9TlvGmMA/x33HfUWBjaF9hMMRpL4tTZlKBUh+cThwfs9q7kkJCAtf19P/6oWyB31twIY6+NNoZ88vow3w2qwfGRD7aV5/hz+I5FFrKWLtb5xHPexJveSe6d4acxIxUoaccfyfAYTHAPTwoy/GCIMM5SgKWO2qMODzgYSFxF+4RfCc8ncuS+V2OOpqIA9l1GRPvI8MPRdPUj7VqJwJqijEOnk+VAi8gyeFqffoLQIwXMj6glpxJXB82ovWBuxCZRU83jFoAkJLqVuLpxQczhGXkvikdmGc3ffYUkS1kfBIGYVbXX3ELMh+e7T9Yny0VSMpTwjlZx7OxzNVg3ZkiDXf23vMpE4IBZVF5YUKcadQ/h8N1MDmk4R8TLSxb2QRPUmLmj3W6XCjKabeqMmfNAuyLPrOgXZ4J4D9Kdu9nXWD3Sn00RDhTC3Cj7PUvq0NHT2E5rugWDWJvt9yIQemt/YZssOeZijXS4Lk6qllF6j/HwT3gUJ5siK3M6ZnOP1t7mcRyRES68p2wcKiNfMl+kESwXDLbeLPVrqYaG2J6AmEe4i/41sa21G/CzEgGaOKzBtRKaB7nhZij1OUvvZO78UnJBQO/+SG10wH010K6OO9xP5db0++83OAtrE3u/6kSI7fxHXpTRCdHnLMn7aya9itGuZRukUR3fnnrljw7HwMgOrbILs/ac11WKsRULX2mzaD1G3hx7osWuLuR88lNPN0CS/g4UiDylKB2AHuZS5+mx8Wa8t5+mHZO/m+7STqAZ00rWOWVp+b1dr51OjN4bpuCbfIcgusIYthyQGNmlVe5reX7Ru9KaM3E+G5fo+/+BQkr/hOBYe/Lhmf6x3eLeABIgdTNmudoPSbN0y7JpvD/cM5uaHLd9fbxhaRNg0Qe87IQXzyWvZIl/2k7soTH8KvLg+RGcMCWdf6EBZ5qMpBrtKmu3ijPZw9cVe+fnQdFe0bvmjnOBKuRzgZsZnSiuVYkP+AgROKP0QxaWT2zmx/w54yd8RIF6vdJI9avHzjlrj8U3CjDOG5xHQluT/euY391AraOLq4PqeKe62TUn7l0cjtR1m6Fk6rbOgRvsXkM9qyjgiHFvvsbMoQlZAILxRqDt/9PCB5fz4k04WJSQyPB3zy0WdmE2sGBZxinl9AgV17vBckXZ5tHmjZc8/NAFmoiOxqhMXnMdpUE4XcCKbnIMlIYT5tNVkWDlAKlEHXplrTc3lN7e/gnhSXmEwT+3Joe3qdCc9tEkX8bSy7wokAfjL8QrpF4aDTBBVp59vzf44VIKtbIJejjagle8mnayiekMMmF9FqO8TZzpLEZSmWUHa//2ZprJQyUcmlLMg5VsbWewYy8XAP/B0FQ5rFixyEIQKN05E5kS/iT/48WTK5z26JNMucV3MhM/L3lMOso+DJc73f9y+OctzP6bQufhlt63PBbJ+pOzduMGK9quinJlUJRFo4v7fW48HW3aHiXFAIiVjEh8LRpZByYgJQ/E9IO3n6dWxcxGZCbJPXShn9hfHZxUauVlFon1fakFLV/90BbuOzDWTbWRBDtxhY06mLLS8hXD6CaQQ+RR+1IxaG68EnRiXGru6ZeACYm6UTKTm0UCTcc+DA/SiWhbjVxVnqDsk/5C5YFsOMnnj6B40wwiFnT6Anh2Hr9c0qlsXNU4UbviDnNMaAadcb2BNmisvOmXyOnJI5ewX6RK0Zmo9GASC1KDN6rkZDPwmhMEsIQY2+/nhzoXlkXwWhmlvMfFwtD51Gzs4AFiHEtxyWzmRLq71rQ3EXranSD9WBuji0Pr1Hp2ZV/cwrJAYM+0LaDcoa3Z91oAGMyA5qiDrt0JgZOymLgWoceR1Bia29rivwMvyIbXEY1qNUD4/gJxbCPsJioGE3oFzi54fKoLtem3QuK4S1No6fF6/ZE48WBr272CWpkAg3RzxF5W0km+41yy4ykV5TtKUO9h3cbtnjW4w8pEYynIqTcJMMOSKvRTF6xC8yQGL3nZR4qGFcszKQibIksby/Ln2PGK0rlyFtgzPd6RgchNBhPWEKPZdvct1UHHThYTesnLxxtvs46hAKycxoWlvIT7f4U6I4TOvtRCFzRV5oXYNXRyddavhZoMkzEaOZSXYQy3RQXZ9JIJdfxuuB5GB2r9KJajnc0GmaQiUctuKQHAdtatRziRkRYFYh5p5SysL19p0v24FEH9FAwyMtFp9H6HU3CH5nLcEcHxhc+DgnZfhxBGFkckal+DcflpdSmaaVjzAgqs6Wm889AZgo9Ggo8abAXvBc8gKNk39js9pEUoDEe/IkhKA6aK65w/SDQSy57jiu+txHcXdzLn4wwYtnhOr2zNcjJ5xhK626hCwnPud2BdwXNECNLge4wq72ahvDHZFuMiVmIaUhk5ltgry2N13CqLzlakfMm0D1EF/upjlby5JcmagdVMQL9rN7dQwBPqnXbKQYToSUHC8OEwZh9NpHb+60gVzl+CVJOTE18+l81sLvBCvqw5a0DP1Eo/mpU8sqDOGK5k4yjF99zhVepIKjslAlMSh1gZW1iT/HQHBzTq2x7Jdeb8zBPNCSdeKG7Z1ZDM9PKGLI3OzZ26LiWFvCR/SBZxqh+w9evfewn+BVq8EoDvYZAueie1ZL3oLt7ismtRguUp8I/jysoVPQ9zcQ93uy5mQ6V+SghsX+OFMbB3sfpw60+hceCsBQEW59pU/JQFp+MdxV7AZcsNWz3exNG/uI3p2D73F9VJIf2ELKc9iLJaREE433L1gMrLorzZMyPh2R2h2LsowSpXkM/LiGpwWlO+cfGmlswqtSQgSpgHCMJJ1ISeNe5syBVl1J7EPikOqyIEDJKMfNTMYfmQm3Zclu3CZVGz4D/iOXdgiOvbmxJNDwtizUvQADHwvjc/Lizqxq8C9fL3e72DE4HYp1bZQuRX9yhY+L/TJrZAQRF4gboj98GQtEY1F/gvDq8q0sVR6iudXCRcRm+yKRgqSNN3NAsJXquhs2NM5sMUskm79jWaGBb0cDQmBwHIcEnauidve9iIDw80a4ULwk9/84i0roIggC7WCIysPudIdvw5rkktDJ/GqvY0Idj6eG+cq3g+1hXdPzRYU7kHxmIDHYMn86K0VyCYEnjz4zFTPkUj6pbDweieDqm9ba+8IvA3eHji4uMHeiuMgGC/bMtlrLTl89oGEExBlp1AiupRVriJbT5XK5npcVlilXV6spgdA9YiF6d4rqOPM/E1WV9px9ZVyHezS8KwEFaY8qfyyshkK8YZguqzinYqnH509iGefNs2bSM5c2YQrqZXrfx6A3gq+S0TCCQ6YxCsaLpnI41M9BCn/gTSzJsBi9xGNQbClaoTB77DRSBXC7cUrMjUlgCYNO4/dcUIG3GkrtJYxcTkH1nu2fQ0JNxqWzQLM0O3dafXb4cOGR97J6JaGjum74fPmbyZ4mWp8gH1zIc51e62JuW4uEAm6+JGElB5dkBpCSZKat2J5h9pTvaj+snUwGRjnmq0hpCXz/H1gmhfMmjOI6pRdIPV4Hb5IPf/bRwpIyKIimDWFAODUYe0PM2gy5dekwfJWR0t9YcDU6+onO1ZqCKi6qV1inquvCMmNaZmOOLSSXYvjKPw6tu5cyMJWeOPEPIaClnqBoCvTcW1IpXxfTGAO4kcWpK8N+egfknCAJcTHJpAZD91r+MS2DBCQTOcf1xpUDi+xpzB1qr9m3BtDQOOY4gZpcpnJarinLcYUg9Bg4ozFE+fM7c5Hw+81lila+kOGHF2RVP8Krwee0D2nBvtIKLoR4FoPTMGGT1lTykfpebsnQVaE5YpFFxkdQwUDCBvADuL7VmSUqO8TUbarQcTl2ZuDf+5ffHwScVqpFYjmd44vbWJA',
+        salt = '3a12bd0ba37ed9d0cf3308619c6fe0f4',
+        isRememberEnabled = true,
+        rememberDurationInDays = 30; // 0 means forever
+
+    // constants
+    var rememberPassphraseKey = 'staticrypt_passphrase',
+        rememberExpirationKey = 'staticrypt_expiration';
+
+    /**
+     * Decrypt a salted msg using a password.
+     * Inspired by https://github.com/adonespitogo
+     *
+     * @param  encryptedMsg
+     * @param  hashedPassphrase
+     * @returns 
+     */
+    function decryptMsg(encryptedMsg, hashedPassphrase) {
+        var iv = CryptoJS.enc.Hex.parse(encryptedMsg.substr(0, 32))
+        var encrypted = encryptedMsg.substring(32);
+
+        return CryptoJS.AES.decrypt(encrypted, hashedPassphrase, {
+            iv: iv,
+            padding: CryptoJS.pad.Pkcs7,
+            mode: CryptoJS.mode.CBC
+        }).toString(CryptoJS.enc.Utf8);
+    }
+
+    /**
+     * Decrypt our encrypted page, replace the whole HTML.
+     *
+     * @param  hashedPassphrase
+     * @returns 
+     */
+    function decryptAndReplaceHtml(hashedPassphrase) {
+        var encryptedHMAC = encryptedMsg.substring(0, 64),
+            encryptedHTML = encryptedMsg.substring(64),
+            decryptedHMAC = CryptoJS.HmacSHA256(encryptedHTML, CryptoJS.SHA256(hashedPassphrase).toString()).toString();
+
+        if (decryptedHMAC !== encryptedHMAC) {
+            return false;
+        }
+
+        var plainHTML = decryptMsg(encryptedHTML, hashedPassphrase);
+
+        document.write(plainHTML);
+        document.close();
+
+        return true;
+    }
+
+    /**
+     * Salt and hash the passphrase so it can be stored in localStorage without opening a password reuse vulnerability.
+     *
+     * @param  passphrase
+     * @returns 
+     */
+    function hashPassphrase(passphrase) {
+        return CryptoJS.PBKDF2(passphrase, salt, {
+            keySize: 256 / 32,
+            iterations: 1000
+        }).toString();
+    }
+
+    /**
+     * Clear localstorage from staticrypt related values
+     */
+    function clearLocalStorage() {
+        localStorage.removeItem(rememberPassphraseKey);
+        localStorage.removeItem(rememberExpirationKey);
+    }
+
+    // try to automatically decrypt on load if there is a saved password
+    window.onload = function () {
+        if (isRememberEnabled) {
+            // show the remember me checkbox
+            document.getElementById('staticrypt-remember-label').classList.remove('hidden');
+
+            // if we are login out, clear the storage and terminate
+            var queryParams = new URLSearchParams(window.location.search);
+
+            if (queryParams.has("staticrypt_logout")) {
+                return clearLocalStorage();
+            }
+
+            // if there is expiration configured, check if we're not beyond the expiration
+            if (rememberDurationInDays && rememberDurationInDays > 0) {
+                var expiration = localStorage.getItem(rememberExpirationKey),
+                    isExpired = expiration && new Date().getTime() > parseInt(expiration);
+
+                if (isExpired) {
+                    return clearLocalStorage();
+                }
+            }
+
+            var hashedPassphrase = localStorage.getItem(rememberPassphraseKey);
+
+            if (hashedPassphrase) {
+                // try to decrypt
+                var isDecryptionSuccessful = decryptAndReplaceHtml(hashedPassphrase);
+
+                // if the decryption is unsuccessful the password might be wrong - silently clear the saved data and let
+                // the user fill the password form again
+                if (!isDecryptionSuccessful) {
+                    return clearLocalStorage();
+                }
+            }
+        }
+    }
+
+    // handle password form submission
+    document.getElementById('staticrypt-form').addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        var passphrase = document.getElementById('staticrypt-password').value,
+            shouldRememberPassphrase = document.getElementById('staticrypt-remember').checked;
+
+        // decrypt and replace the whole page
+        var hashedPassphrase = hashPassphrase(passphrase);
+        var isDecryptionSuccessful = decryptAndReplaceHtml(hashedPassphrase);
+
+        if (isDecryptionSuccessful) {
+            // remember the hashedPassphrase and set its expiration if necessary
+            if (isRememberEnabled && shouldRememberPassphrase) {
+                window.localStorage.setItem(rememberPassphraseKey, hashedPassphrase);
+
+                // set the expiration if the duration isn't 0 (meaning no expiration)
+                if (rememberDurationInDays > 0) {
+                    window.localStorage.setItem(
+                        rememberExpirationKey,
+                        (new Date().getTime() + rememberDurationInDays * 24 * 60 * 60 * 1000).toString()
+                    );
+                }
+            }
+        } else {
+            alert('Hasło niepoprawne!');
+        }
+    });
